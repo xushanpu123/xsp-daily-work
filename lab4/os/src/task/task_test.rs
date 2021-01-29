13f77ae07c1511ddeab9cfe1a4fbacff547af390
xushanpu123 <798304030@qq.com>
2021-01-29 22:21:49 +0800

    lab4

--- lab4/os/src/task/task_test.rs
+++ lab4/os/src/task/task_test.rs
@@ -1,5 +1,7 @@
-use crate::println;
+use core::usize;
+use crate::{console, println};
 use super::structs::*;
+static mut root:Task = Task{context:Context{content_addr:0},kstack:KernelStack(0)};
 extern  "C"{
     pub fn bootstacktop();
     fn t1stacktop();
@@ -7,30 +9,36 @@ extern  "C"{
     fn t3stacktop();
 
 }
-pub unsafe fn task_test(){
-    println!("start testing!");
-    let mut task1 = Task::new(test1 as usize,t1stacktop as usize);
-    //let mut task2 = Task::new(test2 as usize,t2stacktop as usize);
-    //let mut task3 = Task::new(test3 as usize,t3stacktop as usize);
-    let mut current_task = Task::new(0,bootstacktop as usize);
-    current_task.switch_to(&mut task1);
-    println!("I'm coming from task3");
-
-}
 pub unsafe extern "C" fn test1(){
     println!("I'm task1,I love you!");
     let mut task2 = Task::new(test2 as usize,t2stacktop as usize);
-    let mut current_task = Task::new(0,t1stacktop as usize);
-    current_task.switch_to(&mut task2);
+    let mut current_task = Task::new(test1 as usize,t1stacktop as usize);
+    current_task.switch_to(&mut root);
 }
 
 pub unsafe extern "C" fn test2(){
     println!("I'm task2,It's my turn!");
-    let mut task3 = Task::new(test3 as usize,t1stacktop as usize);
+    let mut task3 = Task::new(test3 as usize,t3stacktop as usize);
     let mut current_task = Task::new(0,t2stacktop as usize);
-    current_task.switch_to(&mut task3);
+    current_task.switch_to(&mut root);
 }
 
 pub unsafe extern "C" fn test3(){
     println!("I'm task3,hello world!");
+    let mut task3 = Task::new(test3 as usize,t3stacktop as usize);
+    let mut current_task = Task::new(0,t3stacktop as usize);
+    current_task.switch_to(&mut root);
+}
+pub unsafe  fn task_test(){
+    println!("start testing!");
+    let mut task1 = Task::new(test1 as usize,t1stacktop as usize);
+    root = Task::new(0,bootstacktop as usize);
+    root.switch_to(&mut task1);
+    println!("I'm coming from task1");
+    let mut task2 = Task::new(test2 as usize,t2stacktop as usize);
+    root.switch_to(&mut task2);
+    println!("I'm coming from task2");
+    let mut task3 = Task::new(test3 as usize,t3stacktop as usize);
+    root.switch_to(&mut task3);
+    println!("I'm coming from task3");
 }
